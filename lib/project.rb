12d4805fbcc755f8da8f53e01cd91919ff4a3abc
Nate Tan <nktan93@gmail.com>
2019-04-04 21:07:50 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,6 +8,8 @@ class Project
   
   def add_backer(backer)
     @backers << backer
-    #backer.back_project(self)
+    if backer.backed_projects !include?(self)
+      project.add_backer(self)
+    end
   end
 end
